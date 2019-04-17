50cbf1a18a4c6483d542b076379e6955137a8aa5
Oreooo <yuan80056@gmail.com>
2019-04-17 12:00:55 +0800

    创建kotlin分支

---
+++ app/src/main/java/com/example/oreooo/todoforstudy/entity/Project.kt
@@ -0,0 +1,8 @@
+package com.example.oreooo.todoforstudy.entity
+
+import java.util.*
+
+data class Project(var addTime:String, var thePlan:String, var uuid: UUID,
+                   var doneTime:String, var doneDate: String, var done:Int)
+
+
