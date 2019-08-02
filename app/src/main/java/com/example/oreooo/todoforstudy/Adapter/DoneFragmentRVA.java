ad3d329980bf980969d9f4cda43648e04234a201
Oreooo <yuan80056@gmail.com>
2019-08-02 21:33:30 +0800

    上传代码

--- app/src/main/java/com/example/oreooo/todoforstudy/Adapter/DoneFragmentRVA.java
+++ app/src/main/java/com/example/oreooo/todoforstudy/Adapter/DoneFragmentRVA.java
@@ -7,19 +7,18 @@ import android.widget.TextView;
 import com.example.oreooo.todoforstudy.R;
 import com.example.oreooo.todoforstudy.LItePalDB.Project;
 import com.oreooo.library.ListBase.BaseRecyclerAdapter;
-import com.oreooo.library.ListBase.BaseViewHolder;
 
 import java.util.List;
 
 public class DoneFragmentRVA extends BaseRecyclerAdapter<Project> {
 
     public DoneFragmentRVA(Context context, List<Project> list, int layoutId,
-                           @Nullable OnItemClickListener listener) {
+                           @Nullable OnViewHolderClickListener listener) {
         super(context, list, layoutId, listener);
     }
 
     @Override
-    public void bindHolder(BaseViewHolder holder, Project project) {
+    public void bindHolder(ViewHolder holder, Project project) {
         TextView textView = (TextView)holder.getView(R.id.txt_description);
         String done = String.valueOf(holder.getPosition() + 1) + ". " + project.getThePlan();
         textView.setText(done);
